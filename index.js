248c9bf8d4b447a1ab30a498a599a6ab64f7c95e
Allyson Wesman <allysonwesman@gmail.com>
2017-12-22 05:48:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var name = "Susan"
-var height = "74"
+var height = "70"
 
-if (name === "Susan") {
+/* if (name === "Susan") {
 console.log("Name is correct!")
 }
 else {
   console.log("Expected name to be Susan")
-}
+}*/
 
 
 // Don't worry about this
